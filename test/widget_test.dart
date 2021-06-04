f4a4e74642720423fdac5b46fa424f3bf86fd0de
reesebre82 <reesebre82@gmail.com>
2021-06-04 12:21:58 -0500

    test requreid for ci

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,9 +11,9 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:website/main.dart';
 
 void main() {
-  // test('testing tests', () async {
-  //   expect(1, 1);
-  // });
+  test('testing tests', () async {
+    expect(1, 1);
+  });
 
   // test('testing fail', () async {
   //   expect(2, 1);
