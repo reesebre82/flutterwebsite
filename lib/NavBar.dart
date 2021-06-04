2e4bb62911fb00ea9dbea91be395f8a06a7def13
reesebre82 <reesebre82@gmail.com>
2021-06-04 18:17:26 -0500

    added navbar and animations to navbar

---
+++ lib/NavBar.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:website/NavBarItem.dart';
+
+class NavBar extends StatefulWidget {
+  @override
+  _NavBarState createState() => _NavBarState();
+}
+
+class _NavBarState extends State<NavBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height,
+      width: 150,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Container(
+            width: 150,
+            height: 150,
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.grey),
+              color: Colors.white,
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(5),
+                  child: Text(
+                    "Brendan Reese",
+                    style: TextStyle(
+                      fontSize: 17.5,
+                      color: Color(0xff004E64),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(5),
+                  child: Text(
+                    "Software Developer",
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Color(0xff00A5CF),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          NavBarItem(
+              title: "About",
+              icon: Icons.person,
+              themeColor: Color(0xff004E64)),
+          NavBarItem(
+              title: "Projects",
+              icon: Icons.account_tree,
+              themeColor: Color(0xff00A5CF)),
+          NavBarItem(
+              title: "Skills",
+              icon: Icons.library_books,
+              themeColor: Color(0xff9FFFCB)),
+          NavBarItem(
+              title: "Education",
+              icon: Icons.school,
+              themeColor: Color(0xff25A18E)),
+          NavBarItem(
+              title: "Contact",
+              icon: Icons.contact_mail,
+              themeColor: Color(0xff7AE582)),
+          NavBarItem(
+              title: "Resume",
+              icon: Icons.description,
+              themeColor: Color(0xffC8AD55)),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/NavBarItem.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+class NavBarItem extends StatefulWidget {
+  final String title;
+  final IconData icon;
+  final Color themeColor;
+
+  NavBarItem(
+      {required this.title, required this.icon, required this.themeColor});
+
+  //ADD STATE AND APPROVED EVERYWHERE
+
+  @override
+  _NavBarItemState createState() => _NavBarItemState();
+}
+
+class _NavBarItemState extends State<NavBarItem> with TickerProviderStateMixin {
+  bool isHovered = false;
+  var color = Colors.white;
+
+  late AnimationController controller;
+  late Animation<double> animation;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = new AnimationController(
+        duration: Duration(milliseconds: 250), vsync: this)
+      ..addListener(() => setState(() {}));
+    animation = Tween(begin: 0.0, end: 5.0).animate(controller);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (isHovered) {
+      color = Colors.white;
+      controller.forward();
+    } else {
+      color = Colors.white;
+      controller.reverse();
+    }
+    return Stack(
+      children: [
+        Container(
+          width: 150,
+          height: 45,
+          color: widget.themeColor,
+        ),
+        Transform.translate(
+          offset: Offset(animation.value, 0.0),
+          child: Container(
+            height: 45,
+            color: color,
+            child: InkWell(
+              onTap: () {
+                print("score");
+              },
+              onHover: (value) {
+                setState(() {
+                  isHovered = value;
+                });
+              },
+              child: Padding(
+                padding: EdgeInsets.only(top: 10, left: 20, bottom: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Icon(
+                      widget.icon,
+                      color: widget.themeColor,
+                    ),
+                    Container(width: 15),
+                    Text(widget.title),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
