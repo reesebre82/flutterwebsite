ce7359a85ff0fbc6c155794caf7b52b78e84f01f
reesebre82 <reesebre82@gmail.com>
2021-06-11 17:55:58 -0500

    First rough draft of Education

--- lib/containers/Education/EducationContainer.dart
+++ lib/containers/Education/EducationContainer.dart
@@ -3,6 +3,7 @@ import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:flutter_sticky_header/flutter_sticky_header.dart';
 import 'package:timeline_tile/timeline_tile.dart';
+import 'package:website/containers/Education/EducationTimelineBuilder.dart';
 
 import '../Header.dart';
 
@@ -18,77 +19,13 @@ class EducationContainer extends StatelessWidget {
         delegate: SliverChildListDelegate(
           [
             Padding(
-              padding: const EdgeInsets.symmetric(vertical: 50.0),
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 16.0, vertical: 100.0),
               child: Container(
                 height: 250,
-                child: ListView(
-                  scrollDirection: Axis.horizontal,
-                  shrinkWrap: true,
-                  children: [
-                    TimelineTile(
-                      axis: TimelineAxis.horizontal,
-                      alignment: TimelineAlign.manual,
-                      lineXY: 0.3,
-                      isFirst: true,
-                      indicatorStyle: const IndicatorStyle(
-                        height: 20,
-                        color: Colors.black,
-                      ),
-                      beforeLineStyle: const LineStyle(
-                        color: Color(0xff25A18E),
-                        thickness: 6,
-                      ),
-                    ),
-                    const TimelineDivider(
-                      axis: TimelineAxis.vertical,
-                      begin: 0.3,
-                      end: 0.7,
-                      thickness: 6,
-                      color: Color(0xff25A18E),
-                    ),
-                    Container(
-                      width: 300,
-                      child: TimelineTile(
-                        axis: TimelineAxis.horizontal,
-                        alignment: TimelineAlign.manual,
-                        lineXY: 0.7,
-                        beforeLineStyle: const LineStyle(
-                          color: Color(0xff25A18E),
-                          thickness: 6,
-                        ),
-                        afterLineStyle: const LineStyle(
-                          color: Color(0xff25A18E),
-                          thickness: 6,
-                        ),
-                        indicatorStyle: const IndicatorStyle(
-                          height: 20,
-                          color: Colors.deepOrange,
-                        ),
-                        endChild: Text("testinig"),
-                      ),
-                    ),
-                    const TimelineDivider(
-                      axis: TimelineAxis.vertical,
-                      begin: 0.3,
-                      end: 0.7,
-                      thickness: 6,
-                      color: Color(0xff25A18E),
-                    ),
-                    TimelineTile(
-                      axis: TimelineAxis.horizontal,
-                      alignment: TimelineAlign.manual,
-                      lineXY: 0.3,
-                      isLast: true,
-                      beforeLineStyle: const LineStyle(
-                        color: Color(0xff25A18E),
-                        thickness: 6,
-                      ),
-                      indicatorStyle: const IndicatorStyle(
-                        height: 20,
-                        color: Colors.black,
-                      ),
-                    ),
-                  ],
+                child: EducationTimelineBuilder(
+                  dataStamp: ["etst"],
+                  timeStamp: ["test"],
                 ),
               ),
             ),
