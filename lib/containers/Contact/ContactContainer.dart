5912d5262d8bfd61d361ac44e1c4bf1fb333153f
reesebre82 <reesebre82@gmail.com>
2021-06-11 14:21:26 -0500

    completed contact section -first draft

--- lib/containers/Contact/ContactContainer.dart
+++ lib/containers/Contact/ContactContainer.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
 import 'package:flutter_sticky_header/flutter_sticky_header.dart';
 import '../Header.dart';
+import 'ContactSection.dart';
 
 class ContactContainer extends StatelessWidget {
   @override
@@ -13,9 +15,47 @@ class ContactContainer extends StatelessWidget {
       sliver: SliverList(
         delegate: SliverChildListDelegate(
           [
-            Image(
-              image: AssetImage('images/chalkboard_web.jpg'),
-              fit: BoxFit.fill,
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                    child: ContactSection(
+                      title: "Github",
+                      dataTitle: ["GitHub Link: \nReesebre82"],
+                      data: ["https://github.com/reesebre82/"],
+                      icon: Foundation.social_github,
+                      isLink: true,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                    child: ContactSection(
+                      title: "Email",
+                      dataTitle: ["Preferred Email:", "Alternate: "],
+                      data: [
+                        "brendan@brendanreese.com",
+                        "reesebre82@gmail.com"
+                      ],
+                      icon: Foundation.mail,
+                      isLink: false,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                    child: ContactSection(
+                      title: "Phone Number",
+                      dataTitle: ["Mobile:"],
+                      data: ["1(512)900-0702"],
+                      icon: Foundation.mail,
+                      isLink: false,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
