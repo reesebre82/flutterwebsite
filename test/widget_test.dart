a114b62b5517ca74a1158b4ac92f6cb4eac2df03
reesebre82 <reesebre82@gmail.com>
2021-06-30 01:17:53 -0500

    removed clutter

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,14 +8,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:website/main.dart';
-
 void main() {
   test('testing tests', () async {
     expect(1, 1);
   });
-
-  // test('testing fail', () async {
-  //   expect(2, 1);
-  // });
 }
