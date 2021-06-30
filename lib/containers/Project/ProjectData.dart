f3b91e7a20e8a7a26d822a67e26fc15bba70a29e
reesebre82 <reesebre82@gmail.com>
2021-06-30 01:15:45 -0500

    Data used for project content

---
+++ lib/containers/Project/ProjectData.dart
@@ -0,0 +1,5 @@
+class ProjectData {
+  static List<String> titles = ['Reel Casters', 'AI Sudoku Solver', 'test'];
+  static List<String> shortDescriptions = ['test', 'test', 'test'];
+  static List<String> descriptinos = [];
+}
