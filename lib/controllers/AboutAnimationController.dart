94f96dca56c68abd54036896a8cc3d776e05b0dd
reesebre82 <reesebre82@gmail.com>
2021-08-15 12:46:12 -0500

    removing hard code

--- lib/controllers/AboutAnimationController.dart
+++ lib/controllers/AboutAnimationController.dart
@@ -4,8 +4,13 @@ class AboutAnimationController extends ChangeNotifier {
   List<bool> animated = [false, false, false, false, false];
   double _scrollPosition = 0.0;
   double screenHeight = 0.0;
+  double toggleHeight = 10000.0;
 
   double get scrollPosition => _scrollPosition;
+
+  void setToggleHeight(double height) {
+    toggleHeight = height;
+  }
 
   bool getAnimated(int index) {
     return animated[index];
@@ -17,7 +22,7 @@ class AboutAnimationController extends ChangeNotifier {
 
   checkAnimation() {
     if (animated[0] == false) {
-      if (scrollPosition >= 1190) {
+      if (scrollPosition >= toggleHeight) {
         animated[0] = true;
         Future.delayed(Duration(milliseconds: 500), () {
           completeAnimation();
